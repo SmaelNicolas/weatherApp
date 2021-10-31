6de87e428570208c173567f46a5b76013a372293
Smael Nicolas <nico.smael@gmail.com>
2021-10-31 17:21:48 -0300

    set api calls

--- script.js
+++ script.js
@@ -13,14 +13,60 @@ fetch(WEATHERURL)
     alert("fallo");
   });
 
-// const setIcon = (icon) => {
-//   document
-//     .getElementById("icon")
-//     .setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
-//   console.log(`http://openweathermap.org/img/wn/${icon}@2x.png`);
-// };
+const setIcon = (icon) => {
+  document
+    .getElementById("wIcon")
+    .setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+  console.log(`http://openweathermap.org/img/wn/${icon}@4x.png`);
+};
+
+const setCityName = (cityName) => {
+  document.getElementById("cityName").innerHTML = cityName;
+};
+
+const setTemp = (temperature) => {
+  document.getElementById("temp").innerHTML = temperature;
+};
+const setFeelsLike = (feelsLike) => {
+  document.getElementById("feelsLike").innerHTML = feelsLike;
+};
+const setHumidiy = (humidity) => {
+  document.getElementById("humidity").innerHTML = humidity;
+};
+const setWind = (wind) => {
+  document.getElementById("wind").innerHTML = wind;
+};
+const setClouds = (clouds) => {
+  document.getElementById("clouds").innerHTML = clouds;
+};
+const setWeatherDescription = (weatherDescription) => {
+  document.getElementById("weather").innerHTML = weatherDescription;
+};
+const setCoords = (coordLat, coordLon) => {
+  document.getElementById("lat").innerHTML = coordLat;
+  document.getElementById("lon").innerHTML = coordLon;
+};
 
 const APP = (data) => {
   let icon = data.weather[0].icon;
-  // setIcon(icon);
+  let cityName = data.name;
+  let temperature = data.main.temp;
+  let feelsLike = data.main.feels_like;
+  let humidity = data.main.humidity;
+  let wind = data.wind.speed;
+  let clouds = data.clouds.all;
+  let weatherDescription = data.weather[0].description;
+  let coordLat = data.coord.lat;
+  let coordLon = data.coord.lon;
+
+  setCityName(cityName);
+  setTemp(temperature);
+  setFeelsLike(feelsLike);
+  setHumidiy(humidity);
+  setWind(wind);
+  setClouds(clouds);
+  setWeatherDescription(weatherDescription);
+  setCoords(coordLat, coordLon);
+
+  setIcon(icon);
 };
